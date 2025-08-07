8871afbca9823bd3e0d06a430000c6ec079021a2
Lucas Ayala <164562166+lucasayala1@users.noreply.github.com>
2025-08-07 19:38:20 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando o array com 5 processos
+        Processo[] processos = new Processo[5];
+
+        processos[0] = new Processo("Cadastro", 3);
+        processos[1] = new Processo("Login", 5);
+        processos[2] = new Processo("Pagamento", 6);
+        processos[3] = new Processo("Entrega", 4);
+        processos[4] = new Processo("Suporte", 7);
+
+        int somaEtapas = 0;
+
+        System.out.println("Processos com mais de 4 etapas:");
+        for (int i = 0; i < processos.length; i++) {
+            somaEtapas += processos[i].etapas;
+
+            if (processos[i].etapas > 4) {
+                System.out.println("- " + processos[i].nome + " (" + processos[i].etapas + " etapas)");
+            }
+        }
+
+        double media = (double) somaEtapas / processos.length;
+        System.out.println("\nMédia de etapas: " + media);
+    }
+}
+
+class Processo {
+    String nome;
+    int etapas;
+
+    public Processo(String nome, int etapas) {
+        this.nome = nome;
+        this.etapas = etapas;
+    }
+}
